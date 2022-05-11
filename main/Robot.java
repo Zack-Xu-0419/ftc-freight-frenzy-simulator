f805831d8d8054d29acd0423aeea471d6fa522dd
Zack Xu <zackxu419@outlook.com>
2022-05-11 15:04:46 -0400

    Add Position and Orientation of Bot

--- main/Robot.java
+++ main/Robot.java
@@ -1,10 +1,27 @@
 public class Robot {
-    private int position = 0;
+    private int position[] = { 0, 0 };
     private int sizeX = 1; // Pixel Size from center of the robot to the edge
     private int sizeY = 1; // Pixel Size from center of the robot to the edge
+    private int orientation = 0; // Degree of rotation. 0 means Slide is facing to the other alliance
 
-    public boolean move(int amount, int direction){
+    public boolean move(int amount, int direction) {
         // Return true if the robot can be moved in a certain direction
         return true;
+    }
+
+    public int[] getPosition() {
+        return position;
+    }
+
+    public int getSizeX() {
+        return sizeX;
+    }
+
+    public int getSizeY() {
+        return sizeY;
+    }
+
+    public int getOrientation() {
+        return orientation;
     }
 }
