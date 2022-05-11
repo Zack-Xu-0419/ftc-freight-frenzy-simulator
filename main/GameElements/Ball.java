d2e176956c3a0da4b4546979cd48ec3f10e7aefb
andrewwang2024 <andrew.wang@sbs.org>
2022-05-11 14:12:46 -0400

    Game Element + Ball Skeleton

--- main/GameElements/Ball.java
+++ main/GameElements/Ball.java
@@ -1,4 +1,8 @@
 package GameElements;
 
-public class Ball {
+public class Ball extends GameElement {
+
+    protected Ball(int x, int y) {
+        super(x, y, 1);
+    }
 }
--- main/GameElements/GameElement.java
+++ main/GameElements/GameElement.java
@@ -1,4 +1,15 @@
 package GameElements;
 
 public class GameElement {
+
+    protected final int weight;
+    protected int x;
+    protected int y;
+
+    protected GameElement(int x, int y, int weight) {
+        this.x = x;
+        this.y = y;
+        this.weight = weight;
+    }
+
 }
