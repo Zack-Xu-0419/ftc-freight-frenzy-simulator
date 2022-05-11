78e85c75a31fce585c915628e208c9aa80e2e985
Zack Xu <zackxu419@outlook.com>
2022-05-11 14:57:46 -0400

    Add Robot and Controller Class

---
+++ main/Robot.java
@@ -0,0 +1,10 @@
+public class Robot {
+    private int position = 0;
+    private int sizeX = 1; // Pixel Size from center of the robot to the edge
+    private int sizeY = 1; // Pixel Size from center of the robot to the edge
+
+    public boolean move(int amount, int direction){
+        // Return true if the robot can be moved in a certain direction
+        return true;
+    }
+}
---
+++ main/keyboardController.java
@@ -0,0 +1,54 @@
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import javax.swing.JFrame;
+import javax.swing.JTextField;
+
+public class keyboardController {
+
+  public static void main(String[] argv) throws Exception {
+
+    JTextField textField = new JTextField();
+
+    textField.addKeyListener(new MKeyListener());
+
+    JFrame jframe = new JFrame();
+
+    jframe.add(textField);
+
+    jframe.setSize(400, 350);
+
+    jframe.setVisible(true);
+
+  }
+}
+
+class MKeyListener extends KeyAdapter {
+
+  @Override
+  public void keyPressed(KeyEvent event) {
+
+    int k = event.getKeyCode();
+
+    if (k == 38) {
+      // UP
+
+      // Check if robot is partially colliding, 
+
+    }
+    if (k == 40) {
+      // Down
+    }
+    if (k == 37) {
+      // Left
+    }
+    if (k == 39) {
+      // Right
+    }
+
+    if (event.getKeyCode() == KeyEvent.VK_HOME) {
+
+      System.out.println("Key codes: " + event.getKeyCode());
+
+    }
+  }
+}
