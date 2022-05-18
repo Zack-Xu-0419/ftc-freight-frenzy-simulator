fd7cdc1bfc21da67c5abe271ef6893c0994aef5e
Zack Xu <zackxu419@outlook.com>
2022-05-18 17:59:16 -0400

    Added Intake and indication for element

--- main/Drawer.java
+++ main/Drawer.java
@@ -7,22 +7,44 @@ public class Drawer {
     public static void drawRobot(Graphics g, Robot robot) {
         Graphics2D g2D = (Graphics2D) g;
         g2D.setColor(Color.BLACK);
-        //get postion of four corners
+        // get postion of four corners
         int leftX = robot.getPosition()[0] - robot.getSizeX();
         int rightX = robot.getPosition()[0] + robot.getSizeX();
         int upperY = robot.getPosition()[1] - robot.getSizeY();
         int lowerY = robot.getPosition()[1] + robot.getSizeY();
-        //rotate the corner and draw the robot
+        // rotate the corner and draw the robot
         g2D.rotate(Math.toRadians(robot.getOrientation()), robot.getPosition()[0], robot.getPosition()[1]);
         g2D.fillPolygon(new int[] { leftX, leftX, rightX, rightX }, new int[] { upperY, lowerY, lowerY, upperY }, 4);
-        //draw the slide
-        if(robot.slideExtended){
+        // draw the slide
+        if (robot.slideExtended) {
             g2D.drawLine(robot.getPosition()[0], robot.getPosition()[1],
-                    robot.getPosition()[0] + robot.getSizeX() + robot.getSlideLength(),
-                    robot.getPosition()[1]);
+                    robot.getPosition()[0],
+                    robot.getPosition()[1] + robot.getSizeX() + robot.getSlideLength());
+        }
+        // Draw intake left
+        g2D.setColor(Color.RED);
+        if (robot.intakeDownLeft) {
+            g2D.fillRect(robot.getPosition()[0] - robot.getSizeX() - 20, robot.getPosition()[1] - robot.getSizeY() / 2,
+                    robot.getSizeX() - 20,
+                    robot.getSizeY());
+        }
+        g2D.setColor(Color.BLUE);
+        if (robot.intakeDownLeft) {
+            g2D.fillRect(robot.getPosition()[0] + robot.getSizeX() + 20 - (robot.getSizeX() - 20),
+                    robot.getPosition()[1] - robot.getSizeY() / 2,
+                    robot.getSizeX() - 20,
+                    robot.getSizeY());
         }
 
-        //rotate back the graphic so that next time it be stadard direction, this will not affect previous drawing
+        // If robot has game element, draw indication
+        if (robot.hasGameElement) {
+            g2D.setColor(Color.GREEN);
+            g2D.fillOval(robot.getPosition()[0] - 15, robot.getPosition()[1] - 15, 30, 30);
+        }
+
+        g2D.setColor(Color.BLACK);
+        // rotate back the graphic so that next time it be stadard direction, this will
+        // not affect previous drawing
         g2D.rotate(-1 * Math.toRadians(robot.getOrientation()), robot.getPosition()[0], robot.getPosition()[1]);
     }
 
@@ -46,9 +68,11 @@ public class Drawer {
         for (Cube cube : field.cubes) {
             g.drawOval(cube.getX(), cube.getY(), 10, 10);
         }
-        /*for (Duck duck : field.ducks) {
-            g.drawOval(duck.getX() - 10, duck.getY(), 20, 20);
-        }*/
+        /*
+         * for (Duck duck : field.ducks) {
+         * g.drawOval(duck.getX() - 10, duck.getY(), 20, 20);
+         * }
+         */
 
         // draw barer
         g.drawRect(100, 275, 700, 50);
@@ -61,7 +85,7 @@ public class Drawer {
         g.fillOval(250, 475, 100, 100);
     }
 
-    //draw the scoring board
+    // draw the scoring board
     public static void drawScore(Graphics g, Field field) {
         g.setColor(Color.BLACK);
         int allianceScore = Scoring.score(field.allianceHub);
--- main/Robot.java
+++ main/Robot.java
@@ -4,10 +4,13 @@ public class Robot {
     private int sizeY = 50; // Pixel Size from center of the robot to the edge
     private int orientation = 0; // Degree of rotation. 0 means Slide is facing to the other alliance
     private int speed = 5;
-    private int slideLength = 50;
+    private int slideLength = 400;
     private int oldX = 0;
     private int oldY = 0;
     public boolean slideExtended = false;
+    public boolean intakeDownLeft = false;
+    public boolean intakeDownRight = false;
+    public boolean hasGameElement = false;
 
     Robot() {
 
@@ -17,8 +20,6 @@ public class Robot {
         this.position[0] = positionX;
         this.position[1] = positionY;
     }
-
-
 
     public void setSizeX(int sizeX) {
         this.sizeX = sizeX;
@@ -62,6 +63,13 @@ public class Robot {
             }
         }
 
+        if ((position[0] > 275 - sizeX && position[0] < 325 + sizeX)
+                || (position[0] > 575 - sizeX && position[0] < 625 + sizeX)) {
+            if (position[1] > 100 - sizeY && position[1] < 275 + sizeY) {
+                speed = 1;
+            }
+        }
+
         // The input 'direction': 1-left, 2-right, 3-up, 4-down
 
         // Return 0 if robot is completely against the wall
@@ -79,13 +87,25 @@ public class Robot {
         double downMostY = position[1]
                 + Math.abs((Math.sqrt(2) * sizeY) * Math.cos(Math.PI / 4 - orientation % 90 * Math.PI / 180));
 
-        if (position[0] - sizeX < (int) (75./2 * Math.sqrt(2)) && position[1] + sizeY > (int) (900 - 75./2 * Math.sqrt(2))) {
+        int X = position[0] - sizeX;
+        int Y = position[1] + sizeY;
+        if (
+        // If collide with carosel
+        (X < (int) (75. / 2 * Math.sqrt(2))
+                && Y > (int) (900 - 75. / 2 * Math.sqrt(2)))
+                ||
+                // If collide with Shared shipping hub
+                (X > 300 && X < 500
+                        && Y > 100 && Y < 300)
+                ||
+                // If collide with Alliance shipping hub
+                (X > 150 && X < 350
+                        && Y > 475 && Y < 675)
+
+        ) {
             position[0] = oldX;
             position[1] = oldY;
         }
-        if ((position[0] - sizeX < (int) (75./2 * Math.sqrt(2)) && position[1] + sizeY > (int) (900 - 75./2 * Math.sqrt(2)))
-           || (position[0] - sizeX < (int) (75./2 * Math.sqrt(2)) && position[1] + sizeY > (int) (900 - 75./2 * Math.sqrt(2))))
-
         oldX = position[0];
         oldY = position[1];
 
